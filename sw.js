135d3d4dbf474f92131847b89c8b52e8a9a55d28
Wnm03 <Wisnunm3@gmail.com>
2026-08-15 15:21:29 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Strategi: network-first (biar update selalu terambil saat online),
 // fallback ke cache saat offline. Precache file inti saat install.
 
-const CACHE_NAME = 'kw-cache-v1352';
+const CACHE_NAME = 'kw-cache-v1354';
 const PRECACHE_URLS = [
   './index.html',
   './app_production.html',
