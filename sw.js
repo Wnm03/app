1ac726ff4b7962a134f4489548d72dec75793bf0
Wnm03 <Wisnunm3@gmail.com>
2026-09-25 06:48:44 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker - Keluarga W
 // S1818: cache only static app assets; keep navigations fresh when online.
-const CACHE_NAME = 'kw-cache-v2011';
+const CACHE_NAME = 'kw-cache-v2000';
 const PRECACHE_URLS = [
   './index.html',
   './app_production.html',
